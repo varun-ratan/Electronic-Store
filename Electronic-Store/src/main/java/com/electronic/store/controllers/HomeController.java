727f23518c0e2bc81d7b6fcb0129e82ebab36918
varun <ratanvarun1995@gmail.com>
2024-01-26 23:33:14 +0530

    Added User Entity

--- Electronic-Store/src/main/java/com/electronic/store/controllers/HomeController.java
+++ Electronic-Store/src/main/java/com/electronic/store/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.electronic.store.controller;
+package com.electronic.store.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ Electronic-Store/src/main/java/com/electronic/store/entities/User.java
@@ -0,0 +1,32 @@
+package com.electronic.store.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    private String userId;
+    @Column(name="user_name")
+    private String name;
+    @Column(name="user_email", unique = true)
+    private String email;
+    @Column(name="user_password", length = 10)
+    private String password;
+    private String gender;
+    @Column(length = 1000)
+    private String about;
+    @Column(name="user_image_name")
+    private String imageName;
+
+
+}
